a3f8ac0e30174eb3aa3ca54525761780c82dbf0d
Tim Swan <timswandev@gmail.com>
2026-01-14 19:59:51 +0000

    added delay to the engine selecting a random move to mimic the delay of properly generating a move

--- engine.cpp
+++ engine.cpp
@@ -1,7 +1,9 @@
 #include "engine.h"
 #include <random>
+#include <thread>
 
 Move Engine::generateEngineMove(std::vector<Move> legalMoves) {
+    std::this_thread::sleep_for(std::chrono::seconds(2));
     std::random_device random;
     std::mt19937 generator(random());
     std::uniform_int_distribution<> range(0, legalMoves.size());
