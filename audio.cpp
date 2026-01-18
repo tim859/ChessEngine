3dfdda865ffd3d9f5a3351ae67eecf4bbd1f8ba1
Tim Swan <timswandev@gmail.com>
2026-01-18 16:23:55 +0000

    added more sound effects

--- audio.cpp
+++ audio.cpp
@@ -1,7 +1,11 @@
 #include "audio.h"
 #include <iostream>
 
-Audio::Audio() : moveSelfSound(moveSelfBuffer), captureSound(captureBuffer), castleSound(castleBuffer) {
+Audio::Audio() : gameStartSound(gameStartBuffer), moveSelfSound(moveSelfBuffer), captureSound(captureBuffer),
+                 castleSound(castleBuffer), promoteSound(promoteBuffer), moveCheckSound(moveCheckBuffer), gameEndSound(gameEndBuffer) {
+    if (!gameStartBuffer.loadFromFile("assets/game-start.mp3"))
+        std::cerr << "Could not load game-start.mp3 into sound buffer" << std::endl;
+
     if (!moveSelfBuffer.loadFromFile("assets/move-self.mp3"))
         std::cerr << "Could not load move-self.mp3 into sound buffer" << std::endl;
 
@@ -10,9 +14,22 @@ Audio::Audio() : moveSelfSound(moveSelfBuffer), captureSound(captureBuffer), cas
 
     if (!castleBuffer.loadFromFile("assets/castle.mp3"))
         std::cerr << "Could not load castle.mp3 into sound buffer" << std::endl;
+
+    if (!promoteBuffer.loadFromFile("assets/promote.mp3"))
+        std::cerr << "Could not load promote.mp3 into sound buffer" << std::endl;
+
+    if (!moveCheckBuffer.loadFromFile("assets/move-check.mp3"))
+        std::cerr << "Could not load move-check.mp3 into sound buffer" << std::endl;
+
+    if (!gameEndBuffer.loadFromFile("assets/game-end.mp3"))
+        std::cerr << "Could not load game-end.mp3 into sound buffer" << std::endl;
 }
 
-void Audio::playSound(const GameTypes::MoveType moveType) {
+void Audio::playSoundOnStart() {
+    gameStartSound.play();
+}
+
+void Audio::playSoundOnMove(const GameTypes::MoveType moveType) {
     switch (moveType) {
         case GameTypes::MoveType::NONE:
             break;
@@ -26,9 +43,13 @@ void Audio::playSound(const GameTypes::MoveType moveType) {
             castleSound.play();
             break;
         case GameTypes::MoveType::PROMOTEPAWN:
-            moveSelfSound.play();
+            promoteSound.play();
+            break;
+        case GameTypes::MoveType::CHECK:
+            moveCheckSound.play();
             break;
         case GameTypes::MoveType::GAMEOVER:
+            gameEndSound.play();
             break;
     }
 }
--- audio.h
+++ audio.h
@@ -4,16 +4,25 @@
 #include <SFML/Audio.hpp>
 
 class Audio {
+    sf::SoundBuffer gameStartBuffer;
+    sf::Sound gameStartSound;
     sf::SoundBuffer moveSelfBuffer;
     sf::Sound moveSelfSound;
     sf::SoundBuffer captureBuffer;
     sf::Sound captureSound;
     sf::SoundBuffer castleBuffer;
     sf::Sound castleSound;
+    sf::SoundBuffer promoteBuffer;
+    sf::Sound promoteSound;
+    sf::SoundBuffer moveCheckBuffer;
+    sf::Sound moveCheckSound;
+    sf::SoundBuffer gameEndBuffer;
+    sf::Sound gameEndSound;
 
 public:
     Audio();
-    void playSound(GameTypes::MoveType moveType);
+    void playSoundOnStart();
+    void playSoundOnMove(GameTypes::MoveType moveType);
 };
 
 #endif //CHESS_AUDIO_H
